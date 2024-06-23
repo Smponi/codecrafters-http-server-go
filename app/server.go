730e871c500b38003f7b705ffa9dbc2371a5546a
Philipp <philipp@MacBook-Pro-von-Philipp.fritz.box>
2024-06-23 10:46:13 +0200

    Echo Endpoint

--- app/server.go
+++ app/server.go
@@ -16,42 +16,65 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 	//
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", "localhost:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	connection, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	// Wrap this in a while loop to handle up to 10 requests
+	for i := 0; i < 10; i++ {
+		connection, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		// Log all information of connection
+		fmt.Println(connection.RemoteAddr().String())
 
-	// Log all information of connection
-	fmt.Println(connection.RemoteAddr().String())
-
-	// Log request target
-	// Read the first line of the request
-	buf := make([]byte, 1024)
-	n, err := connection.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading request: ", err.Error())
-		os.Exit(1)
+		// Log request target
+		// Read the first line of the request
+		buf := make([]byte, 1024)
+		n, err := connection.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading request: ", err.Error())
+			os.Exit(1)
+		}
+		requestLine := string(buf[:n])
+		// Split request line
+		// The String will looke like: "HTTP-Method /path/to/resource HTTP-Version"
+		// We need to split it by space and get the second element
+		requestTarget := strings.Split(requestLine, " ")[1]
+		fmt.Println(requestTarget)
+		answer(requestTarget, connection)
 	}
-	requestLine := string(buf[:n])
-	// Split request line
-	// The String will looke like: "HTTP-Method /path/to/resource HTTP-Version"
-	// We need to split it by space and get the second element
-	requestTarget := strings.Split(requestLine, " ")[1]
-	fmt.Println(requestTarget)
+}
+
+// This function gets the request path
+func answer(requestTarget string, conn net.Conn) {
 	if requestTarget == "/" {
 		fmt.Println("Known target. Returning 200")
 		// Send a HTTP 200 Response with HTTP/1.1
-		connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.HasPrefix(requestTarget, "/echo/") {
+		fmt.Println("ECHO target. ")
+		value := requestTarget[6:]
+		fmt.Println("Value: ", value)
+		// Response should look like this
+		// // Status line
+		// HTTP/1.1 200 OK
+		// \r\n                          // CRLF that marks the end of the status line
+
+		// // Headers
+		// Content-Type: text/plain\r\n  // Header that specifies the format of the response body
+		// Content-Length: 3\r\n         // Header that specifies the size of the response body, in bytes
+		// \r\n                          // CRLF that marks the end of the headers
+
+		// // Response body
+		// abc                           // The string from the request
+		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(value)) + "\r\n\r\n" + value))
 	} else {
 		fmt.Println("Unknown target. Returning 404")
-		connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-
 }
