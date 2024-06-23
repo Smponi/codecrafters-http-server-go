ffe2458e03ca64a17cdadd8d117b0ea84097f4b6
Philipp <philipp@MacBook-Pro-von-Philipp.fritz.box>
2024-06-23 10:28:36 +0200

    Getting ressource path

--- app/server.go
+++ app/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 
 	"net"
@@ -26,6 +28,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Send a HTTP 200 Response with HTTP/1.1
-	connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	// Log all information of connection
+	fmt.Println(connection.RemoteAddr().String())
+
+	// Log request target
+	// Read the first line of the request
+	buf := make([]byte, 1024)
+	n, err := connection.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+		os.Exit(1)
+	}
+	requestLine := string(buf[:n])
+	// Split request line
+	// The String will looke like: "HTTP-Method /path/to/resource HTTP-Version"
+	// We need to split it by space and get the second element
+	requestTarget := strings.Split(requestLine, " ")[1]
+	fmt.Println(requestTarget)
+	if requestTarget == "/" {
+		fmt.Println("Known target. Returning 200")
+		// Send a HTTP 200 Response with HTTP/1.1
+		connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		fmt.Println("Unknown target. Returning 404")
+		connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+
 }
